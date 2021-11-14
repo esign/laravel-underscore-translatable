a511ab2fa46468d14ad80d4044a486350c9b6d23
jordyvanderhaegen <jordy@esign.eu>
2021-11-14 22:06:30 +0100

    Update: apply phpcs

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,8 @@
 
 namespace Esign\UnderscoreTranslatable\Tests;
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
-{}
+{
+}
